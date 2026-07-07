7464d520c3885acfd71a61063524642c95575665
Jooiden <greenplayback@gmail.com>
2026-07-08 03:14:22 +1200

    Delete 5 directory

--- 5/2.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-labels = ['Nokia', 'Samsung', 'Apple', 'Lumia']
-sizes = [10, 30, 45, 15]
-colors = ['yellow', 'green', 'red', 'blue']
-plt.pie(sizes, labels=labels, colors=colors)
-plt.title("Char")
-plt.legend()
-plt.show()
